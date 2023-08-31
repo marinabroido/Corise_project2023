1c56a20aafd3a13fbed44648c91e0578f69ddab0
marinabroido <marinabroido@users.noreply.github.com>
2023-08-31 14:29:50 -0700

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -20,7 +20,7 @@ def main():
         podcast_info = available_podcast_info[selected_podcast]
 
         # Right section - Newsletter content
-        #st.header("Newsletter Content")
+        st.header("Newsletter Content")
 
         # Display the podcast title
         st.subheader("Episode Title")
