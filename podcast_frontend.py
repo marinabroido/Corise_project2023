86ec649f238cb603ccae36d6ce5146f68d2cbfb5
marinabroido <marinabroido@users.noreply.github.com>
2023-08-31 16:44:15 -0700

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -35,9 +35,7 @@ def main():
 
         with col1:
             # Display the podcast episode summary
-            st.write('<p style="font-size:28px; padding-top: 0px;">Podcast Episode Summary</p>',
-                 unsafe_allow_html=True)
-            #st.subheader("Podcast Episode Summary")
+            st.subheader("Podcast Episode Summary")
             st.write(podcast_info['podcast_summary'])
 
         with col2:
