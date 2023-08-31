b59b66be3697330ce851949bd8c89c6c480aa74b
marinabroido <marinabroido@users.noreply.github.com>
2023-08-31 14:12:43 -0700

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -32,7 +32,7 @@ def main():
         
         title = podcast_info['podcast_details']['episode_title']
         #st.write('<p style="font-size:24px">' + title + '</p>',
-            unsafe_allow_html=True)
+            #unsafe_allow_html=True)
         st.write('<p class="font">' + title + '</p>', unsafe_allow_html=True)
 
         # Display the podcast summary and the cover image in a side-by-side layout
