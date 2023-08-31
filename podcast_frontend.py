ab37bc32ede676b6e311bf17d27d142bf707d1ee
marinabroido <marinabroido@users.noreply.github.com>
2023-08-30 19:07:50 -0700

    Update podcast_frontend.py
    testing default formatting of podcast title

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -24,7 +24,8 @@ def main():
 
         # Display the podcast title
         st.subheader("Episode Title")
-        st.write(podcast_info['podcast_details']['episode_title'])
+        title = podcast_info['podcast_details']['episode_title']
+        st.write(f"{title}")
 
         # Display the podcast summary and the cover image in a side-by-side layout
         col1, col2 = st.columns([7, 3])
