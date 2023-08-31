83a2defa0b07b73c128d493a80efcfc08db05725
marinabroido <marinabroido@users.noreply.github.com>
2023-08-31 14:54:42 -0700

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -25,7 +25,7 @@ def main():
         st.write('<p style="font-size:28px;">Episode Title</p>',
         unsafe_allow_html=True)
         st.markdown(""" <style> .font {
-        font-size:36px ; margin-top:0} 
+        font-size:36px ; padding:0} 
         </style> """, unsafe_allow_html=True)
         
         
