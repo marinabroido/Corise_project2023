094c25ed1d4dfdaa2ebacf86c2c076e75e493474
marinabroido <marinabroido@users.noreply.github.com>
2023-08-31 17:56:39 -0700

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -23,7 +23,7 @@ def main():
 
         # Display the podcast title
 
-        st.subheader("Episode Title")
+        #st.subheader("Episode Title")
         title = podcast_info['podcast_details']['episode_title']
         st.write('<p style="font-size:32px;">' + title + '</p>',
             unsafe_allow_html=True)
