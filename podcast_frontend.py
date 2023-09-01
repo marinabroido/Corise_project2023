e9382482a36f47e6b38f6e203931f216521157dd
marinabroido <marinabroido@users.noreply.github.com>
2023-08-31 18:07:12 -0700

    Update podcast_frontend.py
    Edited the added podcast details info (on demand display)

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -22,16 +22,12 @@ def main():
         # Right section - Newsletter content
 
         # Display the podcast title
-
         #st.subheader("Episode Title")
         title = podcast_info['podcast_details']['episode_title']
         st.write('<p style="font-size:32px;padding-top:1rem; padding-bottom:0.5rem;">' + title + '</p>',
             unsafe_allow_html=True)
-        #st.write(podcast_info['podcast_details']['episode_title'])
-
-
-
         
+        #ANOTHER WAY TO FORMAT
         #st.markdown(""" <style> .font {
         #font-size:32px ; padding-top:1rem; padding-bottom:0.5rem;} 
         #</style> """, unsafe_allow_html=True)
@@ -86,37 +82,26 @@ def main():
         # Display the podcast title
         #st.write('<br>', unsafe_allow_html=True)
         st.divider()
-        st.subheader("Episode Title")
+    
+        #st.subheader("Episode Title")
         title = podcast_info['podcast_details']['episode_title']
-        st.write('<p style="font-size:26px; color:red;">' + title + '</p>',
+        st.write('<p style="font-size:32px;padding-top:1rem; padding-bottom:0.5rem;">' + title + '</p>',
             unsafe_allow_html=True)
-        #st.write(podcast_info['podcast_details']['episode_title'])
 
+        st.subheader("Podcast Episode Summary")
         # Display the podcast summary and the cover image in a side-by-side layout
         col1, col2 = st.columns([7, 3])
-
         with col1:
-            # Display the podcast episode summary
-            st.subheader("Podcast Episode Summary")
-            st.write(podcast_info['podcast_summary'])
-
-        with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
-
-        # Display the podcast guest and their details in a side-by-side layout
-        #col3, col4 = st.columns([3, 7])
-
-        #with col3:
-        st.subheader("Podcast Host")
-        st.write(podcast_info['podcast_host'])
-
-        #with col1:
+            st.subheader("Podcast Host")
+            st.write(podcast_info['podcast_host'])
+            
         # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+        with col1:
+            st.subheader("Key Moments")
+            key_moments = podcast_info['podcast_highlights']
+            for moment in key_moments.split('\n'):
+                st.markdown(
+                    f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
 
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
