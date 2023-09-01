8cea54041e5b43927e9a7424122c88cfef947b59
marinabroido <marinabroido@users.noreply.github.com>
2023-08-31 18:36:23 -0700

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -71,7 +71,48 @@ def main():
     process_button = st.sidebar.button("Process Podcast Feed")
     st.sidebar.markdown("**Note**: Podcast processing can take upto 5 mins, please be patient.")
 
-    #if process_button:
+    if process_button:
+     # Call the function to process the URLs and retrieve podcast guest information
+        podcast_info = process_podcast_info(url)
+
+        # Right section - Newsletter content
+        #st.header("Newsletter Content")
+
+        # Display the podcast title
+        #st.write('<br>', unsafe_allow_html=True)
+        st.divider()
+        st.subheader("Episode Title")
+        title = podcast_info['podcast_details']['episode_title']
+        st.write('<p style="font-size:26px; color:red;">' + title + '</p>',
+            unsafe_allow_html=True)
+        #st.write(podcast_info['podcast_details']['episode_title'])
+
+        # Display the podcast summary and the cover image in a side-by-side layout
+        col1, col2 = st.columns([7, 3])
+
+        with col1:
+            # Display the podcast episode summary
+            st.subheader("Podcast Episode Summary")
+            st.write(podcast_info['podcast_summary'])
+
+        with col2:
+            st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
+
+        # Display the podcast guest and their details in a side-by-side layout
+        #col3, col4 = st.columns([3, 7])
+
+        #with col3:
+        st.subheader("Podcast Host")
+        st.write(podcast_info['podcast_host'])
+
+        #with col1:
+        # Display the five key moments
+        st.subheader("Key Moments")
+        key_moments = podcast_info['podcast_highlights']
+        for moment in key_moments.split('\n'):
+            st.markdown(
+                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
+
 
       
 
